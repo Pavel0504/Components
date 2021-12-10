7370359273d7a928b30bbbb3e934d11082f5c50f
Gabidosha <nikitinmworks.com>
2021-12-10 22:04:15 +0700

    layout two

---
+++ Компоненты/Realistic Button Design/main.js
@@ -0,0 +1,9 @@
+const btn = document.querySelectorAll('.btn');
+
+for(let i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function(e) {
+        btn[i].classList.toggle('button-clicked');
+        btn[i].firstElementChild.classList.toggle('button-clicked');
+    })
+}
+
